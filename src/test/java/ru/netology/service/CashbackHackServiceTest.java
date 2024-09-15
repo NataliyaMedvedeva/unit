7459b05f96d6f12a041f72ce9417c8a1d7c41254
Nataliia Medvedeva <shishika87@gmail.com>
2024-09-15 21:29:07 +0700

    Testng

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,54 @@
+package ru.netology.service;
+
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void amountForCashbackMin () {
+        CashbackHackService service = new CashbackHackService ();
+
+        int amount = 999;
+        int surcharge;
+        surcharge = service.remain(amount);
+
+        int expected = 1;
+        int actual = service.remain(amount);
+
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void amountForCashbackBoundary () {
+        CashbackHackService service = new CashbackHackService ();
+
+        int amount = 1000;
+        int surcharge;
+        surcharge = service.remain(amount);
+
+        int expected = 0;
+        int actual = service.remain(amount);
+
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void extraPaymentUntilTheNextCashback () {
+        CashbackHackService service = new CashbackHackService ();
+
+        int amount = 1001;
+        int surcharge;
+        surcharge = service.remain(amount);
+
+        int expected = 999;
+        int actual = service.remain(amount);
+
+
+        assertEquals(actual, expected);
+    }
+}
